d7497db567688cacee8992a82d6270b52ecfa8fa
irikiesh <kumar.atish.br@gmail.com>
2020-01-28 22:36:59 +0530

    initial commit

---
+++ precache-manifest.5aaf746ae93bcadbda6462b77c6583e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb1a0999c65abfde9811c1f5461863f",
+    "url": "/irikeish.io/index.html"
+  },
+  {
+    "revision": "9e3fc477f7d441b5792d",
+    "url": "/irikeish.io/static/css/main.a83e5236.chunk.css"
+  },
+  {
+    "revision": "8f9446066788dda5be91",
+    "url": "/irikeish.io/static/js/2.84c2f952.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/irikeish.io/static/js/2.84c2f952.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e3fc477f7d441b5792d",
+    "url": "/irikeish.io/static/js/main.89cd7e5c.chunk.js"
+  },
+  {
+    "revision": "4069d1af37c8af091c99",
+    "url": "/irikeish.io/static/js/runtime-main.81ae820c.js"
+  }
+]);
